0a038a8f400cb045da883bd9254b6be86f1707e5
Laura Bassani <bassanilaurarita@gmail.com>
2020-10-25 10:50:41 +0100

    Refactoring splicing funcitons for responsabilities

--- main.js
+++ main.js
@@ -1,34 +1,39 @@
 'use strict';
 
-document.getElementById('bola').addEventListener("click", chooseName);
-let names = ["Lucas","Laura","Estefanie","Mercedes","Giacomo","Gabri","Giselle","Laura R","Quim","Olga","Moi","Jose","Jackson","Carmen","Alejandra","Lorena","Loredana","Alvaro","Rene","MaxPower","Isma","Jorge","Sergi P","Vanessa"];
-let selected = [];
-let audio = new Audio('audio/witch.mp3');
-
-function chooseName(){
-    let n = names.length;
-    let i = Math.floor(Math.random()*n);
-    let selectedName = names[i];
-    audio.play();
-    document.getElementById("contestantName").innerHTML = selectedName;
-    names.splice(i, 1);       
-    n--;
-    addSelectedNameToList(selectedName)
-    resetCannoliOfDoom(n)
+window.onload = function() {
+    document.getElementById('bola').addEventListener("click", chooseName);
+    let names = ["Lucas","Laura","Estefanie","Mercedes","Giacomo","Gabri","Giselle","Laura R","Quim","Olga","Moi","Jose","Jackson","Carmen","Alejandra","Lorena","Loredana","Alvaro","Rene","MaxPower","Isma","Jorge","Sergi P","Vanessa"];
+    let selected = [];
+    let audio = new Audio('audio/witch.mp3');
     
-}
-function addSelectedNameToList(selectedName){
-    selected.push("<li>" + selectedName + "</li>"); 
-    document.getElementById("selectedNames").innerHTML = selected.join('');
-}
-
-function resetCannoliOfDoom(namesLength){
-    if(namesLength < 1){
-        names = ["Lucas","Laura","Estefanie","Mercedes","Giacomo","Gabri","Giselle","Laura R","Quim","Olga","Moi","Jose","Jackson","Carmen","Alejandra","Lorena","Loredana","Alvaro","Rene","MaxPower","Isma","Jorge","Sergi P","Vanessa"];
-        selected = [];
+    function chooseName(){
+        let selectedName = takeRandomName();   
+        printInBall(selectedName)
+        addToSelectedNames(selectedName)
+        if (namesIsEmpty()) {
+            resetCannoliOfDoom()
+        }
     }
+    function takeRandomName() {
+        let i = getRandomIndex();
+        return names.splice(i, 1); 
+    }
+    function printInBall(name) {
+        audio.play();
+        document.getElementById("contestantName").innerHTML = name;
+    }
+    function addToSelectedNames(selectedName){
+        selected.push("<li>" + selectedName + "</li>"); 
+        document.getElementById("selectedNames").innerHTML = selected.join('');
+    }
+    function namesIsEmpty() {
+        return names.length < 1
+    }           
+    function resetCannoliOfDoom(){
+        names = ["Lucas","Laura","Estefanie","Mercedes","Giacomo","Gabri","Giselle","Laura R","Quim","Olga","Moi","Jose","Jackson","Carmen","Alejandra","Lorena","Loredana","Alvaro","Rene","MaxPower","Isma","Jorge","Sergi P","Vanessa"];
+        selected = [];        
+    }
+    function getRandomIndex() {
+        return Math.floor(Math.random() * names.length)
+    }    
 }
-
-
-
-
