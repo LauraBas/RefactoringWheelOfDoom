9de8e83ec30ff98a53d1c0ea82cd178994d9b519
Laura Bassani <bassanilaurarita@gmail.com>
2020-10-25 09:54:42 +0100

    Play sound in every click

--- main.js
+++ main.js
@@ -3,11 +3,13 @@
 document.getElementById('bola').addEventListener("click", chooseName);
 let names = ["Lucas","Laura","Estefanie","Mercedes","Giacomo","Gabri","Giselle","Laura R","Quim","Olga","Moi","Jose","Jackson","Carmen","Alejandra","Lorena","Loredana","Alvaro","Rene","MaxPower","Isma","Jorge","Sergi P","Vanessa"];
 let selected = [];
+let audio = new Audio('audio/witch.mp3');
 
 function chooseName(){
     let n = names.length;
     let i = Math.floor(Math.random()*n);
     let selectedName = names[i];
+    audio.play();
     document.getElementById("contestantName").innerHTML = selectedName;
     names.splice(i, 1);       
     n--;
