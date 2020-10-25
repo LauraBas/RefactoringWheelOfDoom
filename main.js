d03c5e9b50835bd23829a2bcc99cdfdec7425dcc
Laura Bassani <bassanilaurarita@gmail.com>
2020-10-25 09:17:05 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+document.getElementById('bola').addEventListener("click", chooseName);
+let names = ["Lucas","Laura","Estefanie","Mercedes","Giacomo","Gabri","Giselle","Laura R","Quim","Olga","Moi","Jose","Jackson","Carmen","Alejandra","Lorena","Loredana","Alvaro","Rene","MaxPower","Isma","Jorge","Sergi P","Vanessa"];
+let selected = [];
+
+function chooseName(){
+    let n = names.length;
+    let i = Math.floor(Math.random()*n);
+    let selectedName = names[i];
+    document.getElementById("contestantName").innerHTML = selectedName;
+
+    names.splice(i, 1);       
+    selected.push("<li>" + selectedName + "</li>"); 
+    document.getElementById("selectedNames").innerHTML = selected.join('');
+    n--;
+
+    if (n < 1){
+        names = selected;
+        selected = [];
+    }
+    
+}
+
+
+
+
